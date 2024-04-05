87dd8fdf66840d976d39dc34c4d9a5450e48e374
Wael Antar <antarwael189@gmail.com>
2024-04-05 15:28:31 +0100

    [Task][0]feat: Project setup

---
+++ src/test/java/com/courses/springbatchtutorial/SpringBatchTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courses.springbatchtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
